25fcba972f913a281d9268c955e01b935384988b
thatphum <pau25441218@gmail.com>
2020-10-20 15:12:32 +0700

    First Commit

---
+++ src/ShuttleBus/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ShuttleBus;
+
+/**
+ *
+ * @author thatphum
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        int[] a ={};
+        System.out.println(a.length);
+    }
+}
---
+++ src/ShuttleBus/ShuttleBus.java
@@ -0,0 +1,49 @@
+package ShuttleBus;
+public class ShuttleBus {
+    String busId = "B#001";
+    int frontDoor = 2;
+    int rearDoor = 2;
+    int onOfPassengers = 7;
+    int odometer = 2400;
+    int money = 0;
+    public  String getBusid(){
+        return this.busId;
+    }
+    public void setBusId(String busId){
+        
+    }
+    public void closeFrontDoor(){
+        
+    }
+    public void openFrontDoor(){
+        
+    }
+    public void closeRearDoor(){
+    
+    }
+    public void openRearDoor(){
+    
+    }
+    public void lockDoor(){
+    
+    }
+    public void addPassengers(int passengers){
+    
+    }
+    public void incrementMoney(int money){
+    
+    }
+    public int getMoney(){
+        return this.money;
+    }
+    public void go(){
+    
+    }
+    public void incrementOdometer(){
+    
+    }
+    public void resetMoney(){
+    
+    }
+    
+}
